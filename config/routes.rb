b582af9c5e46588dda2f0146b6ab4a694c910765
Carlos Aguirre <craguirre952@gmail.com>
2021-11-25 11:53:30 -0500

    create routes for Cocktail controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/cocktails" => "cocktails#index"
+  get "/cocktails/:id" => "cocktails#show"
+  post "/cocktails" => "cocktails#create"
+  patch "/cocktails/:id" => "cocktails#update"
+  delete "/cocktails/:id" => "cocktails#destroy"
+
+
 end
