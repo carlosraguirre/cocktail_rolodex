bf02c01a8e105a1ddadc0636779a662b98afa53a
Carlos Aguirre <craguirre952@gmail.com>
2021-11-25 13:09:59 -0500

    add first record to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Cocktail.create!([
+  {cocktail_name: "Gin Rickey", ingredient: "2 ounces gin, .5 ounces freshly squeezed lime juice, 4 ounces club soda or to taste, Lime wedge for garnish", direction: "Gather the ingredients. Fill a highball glass with ice. Pour the gin and lime juice over the ice. Top woth club soda. Garnish with a lime wedge.", recipe_link: "https://www.thespruceeats.com/gin-rickey-recipe-760083"}
+])
