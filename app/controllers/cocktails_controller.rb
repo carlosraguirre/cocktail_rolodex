2824af83923c4091ae7dace17991a2bd0ef8f264
Carlos Aguirre <craguirre952@gmail.com>
2021-11-25 08:33:29 -0500

    create Cocktail controller actions

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,50 @@
 class CocktailsController < ApplicationController
+
+  def index
+    cocktails = Cocktail.all
+    render json: cocktails
+  end
+
+  def show
+    cocktail = Cocktail.find_by(id: params[:id])
+    render json: cocktail
+  end
+
+  def create
+    cocktail = Cocktail.new(
+      cocktail_name: params[:cocktail_name],
+      ingredient: params[:ingredient],
+      direction: params[:direction],
+      recipe_link: params[:recipe_link]
+    )
+    if cocktail.save
+      render json: cocktail
+    else
+      render json: {error: cocktail.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    cocktail = Cocktail.find_by(id: params[:id])
+
+    cocktail.destroy
+    render json: cocktail
+  end
+
+  def update
+    cocktail = Cocktail.find_by(id: params[:id])
+
+    cocktail.cocktail_name = params[:cocktail_name] || cocktail.cocktail_name
+    cocktail.ingredient = params[:ingredient] || cocktail.ingredient
+    cocktail.direction = params[:direction] || cocktail.direction
+    cocktail.recipe_link = params[:recipe_link] || cocktail.recipe_link
+
+    if cocktail.save
+      render json: cocktail
+    else
+      render json: {error: cocktail.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
 end
+
