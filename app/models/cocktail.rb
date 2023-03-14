1d32ca8e6b3bf7ac3495deced9341f668d611347
Carlos Aguirre <craguirre952@gmail.com>
2023-03-14 19:04:20 -0400

    comment out associations as triage to send API calls from Vue

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,3 +1,3 @@
 class Cocktail < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true
-  has_many :cocktails
+  # has_many :cocktails
 end
