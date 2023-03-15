bec70235fc4df1fa4a643108c0e088333af78a32
Carlos Aguirre <craguirre952@gmail.com>
2023-03-15 13:25:00 -0400

    update cocktails controller to change is_favorite inputs to favorite

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -15,6 +15,7 @@ class CocktailsController < ApplicationController
       ingredient: params[:ingredient],
       direction: params[:direction],
       recipe_link: params[:recipe_link],
+      favorite: params[:favorite],
     )
     if cocktail.save
       render json: cocktail
