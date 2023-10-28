2fea7764ee7cf9cd459ab6c119183a00945ab6f0
Carlos Aguirre <craguirre952@gmail.com>
2023-10-28 14:59:28 -0400

    add favorite to create cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -15,6 +15,7 @@ class CocktailsController < ApplicationController
       ingredient: params[:ingredient],
       direction: params[:direction],
       recipe_link: params[:recipe_link],
+      favorite: params[:favorite],
       user_id: current_user.id,
     )
     # binding.pry
@@ -37,6 +38,7 @@ class CocktailsController < ApplicationController
     cocktail.ingredient = params[:ingredient] || cocktail.ingredient
     cocktail.direction = params[:direction] || cocktail.direction
     cocktail.recipe_link = params[:recipe_link] || cocktail.recipe_link
+    cocktail.favorite = params[:favorite || cocktail.favorite]
 
     if cocktail.save
       render json: cocktail
