891a040e8085d96fc24e188f09ad3ded9c8a5384
Carlos Aguirre <craguirre952@gmail.com>
2023-12-20 11:07:55 -0500

    add show and index for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,14 @@ class UsersController < ApplicationController
       render json: { errors: user.errors.full_messages }, status: :bad_request
     end
   end
+
+  def index
+    users = User.all
+    render json: users
+  end
+
+  def show
+    user = current_user
+    render json: user
+  end
 end
